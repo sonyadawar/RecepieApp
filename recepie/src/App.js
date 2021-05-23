3b3875037ff403dfe18794305eebdbfe2fa4992f
sonyadawar <44790119+sanchitdawar@users.noreply.github.com>
2021-05-23 12:13:15 +0530

    Update all files

--- recepie/src/App.js
+++ recepie/src/App.js
@@ -1,21 +1,26 @@
 import React,{useState,useEffect} from 'react'
 
+import Recipe from './Recipe'
+
 import './App.css';
+
 
 const App=() =>{
 
    const app_id='42ca74f3'
    const app_key='4c83d98accec5ed4ee7682726d4da968'
 
+   const [recipes,setRecipes]=useState([])
+
    useEffect(()=>{
-     getRecipeis();
+     getRecipies();
 
    },[])
 
-   const getRecipeis=async()=>{
+   const getRecipies=async()=>{
      const response=await fetch(`https://api.edamam.com/search?q=chicken&app_id=${app_id}&app_key=${app_key}`)
      const data = await response.json()
-     console.log(data)
+     setRecipes(data.hits)
 
     
 
@@ -28,8 +33,13 @@ return (
       <input type='text'/>
       <button className='search'>Search</button>
 
+
       
     </form>
+    {recipes.map( recipe =>(
+      <Recipe/>
+    ))}
+
 
       
       
---
+++ recepie/src/Recipe.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const recipe=()=>{
+
+
+    return(
+        <div>
+            <h1>Title</h1>
+            <p>Calories</p>
+        </div>
+
+    )
+
+}
+export default recipe
